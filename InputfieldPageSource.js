1e80bcb2a479931a36ba8ecfa1e41dfacb9a222f
Martijn Geerts <martijn.geerts@gmail.com>
2015-01-08 15:27:18 +0100

    First commit

---
+++ InputfieldPageSource.js
@@ -0,0 +1,55 @@
+$(function () {
+
+    'use strict';
+
+    var $copyButton = $(".InputfieldPageSource button"),
+        client = new ZeroClipboard($copyButton),
+        $textarea,
+        $button;
+
+    // copy to clipboad button
+    $copyButton.on('click', function (event) {
+        event.preventDefault();
+    });
+
+    // control
+    client.on('ready', function () {
+
+        // copy
+        client.on('copy', function (event) {
+            $button = $(event.target);
+            $textarea = $button.parent().find('textarea');
+            $button.parent().find('.notes').removeClass('hide');
+            event.clipboardData.setData('text/plain', $textarea.val());
+        });
+
+        // after copy
+        client.on('aftercopy', function (event) {
+            $button = $(event.target);
+            $button.parent().find('.notes').addClass('hide');
+
+            window.setTimeout(function () {
+                $button.removeClass('ui-state-active');
+            }, 330);
+
+        });
+    });
+
+    // destroy & remove unnecessary
+    client.on('error', function () {
+
+        ZeroClipboard.destroy();
+
+        $textarea = $copyButton.parent().find('textarea');
+        $textarea.removeClass('hide');
+
+        // when rows set to 0 and button fails reset the rows of the textarea
+        if (parseInt($textarea.attr('rows'), 10) === 0) {
+            $textarea.attr('rows', 5);
+        }
+
+        $copyButton.remove();
+        $(".InputfieldPageSource .notes").remove();
+    });
+
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Inputfield Page Source
+
+Display a string with the rendered output of this Page (per it's Template) in an Inputfield. The markup could be copied with a button click (flash enabled browsers) and/or copied manually from the Inputfield. The rendered output is a runtime only value which is not stored in the database.
+
+### Potential Use
+Provide an easy way for editors to copy the sourcecode of the page for the use in newsletter services and such.
+
+### Note !
+This Inputfield should **not be used** in the page template directly. _(could give some “funky” results)_
+
+![Inputfield Page Source](sourcecode.png)
